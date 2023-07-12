fbc04b34fddc8b3ce2b85d904e0d817a5c228842
bilal simsek <xyzbilal@gmail.com>
2023-07-12 14:20:34 +0300

    Initial Commit

---
+++ TaskManagementApp/TaskManagementAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  TaskManagementAppApp.swift
+//  TaskManagementApp
+//
+//  Created by Bilal SIMSEK on 12.07.2023.
+//
+
+import SwiftUI
+
+@main
+struct TaskManagementAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
